7b5b46ebcc89ca202c3c87c71ed10162a0b59c51
vincent <vincent.clerc@hunimalis.com>
2021-10-15 16:14:46 +0200

    romain number + modulo

--- src/sum.js
+++ src/sum.js
@@ -20,4 +20,55 @@ function multiply(a, b)
     return a * b;
 }
 
-module.exports = {sum, multiply};
+function modulo(number)
+{
+    let r = '';
+
+    if(number % 3 == 0){
+        r += 'Fizz'; 
+    }
+
+    if(number % 5 == 0){
+        r += 'Buzz'; 
+    }
+
+    if(r == '') {
+        return number;
+    }
+
+    return r;
+}
+
+function romainNumber(number)
+{
+    let latin = '';
+    let symboles = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ]
+
+    symboles.forEach((symbole, i) => {
+
+        while(number >= symbole[0])
+        {
+            number -= symbole[0];
+            latin += symbole[1];
+        }
+
+    });
+
+    return latin;
+}
+
+module.exports = {sum, multiply, modulo, romainNumber};
--- test/sum.test.js
+++ test/sum.test.js
@@ -24,3 +24,138 @@ test('a NAN value', () => {
 test('b NAN value', () => {
   expect(obj.multiply(1, 'test')).toBe(false);
 });
+
+
+
+
+
+
+test('modulo enter 1', () => {
+  expect(obj.modulo(1)).toBe(1);
+});
+
+test('modulo enter 2', () => {
+  expect(obj.modulo(2)).toBe(2);
+});
+
+test('modulo enter 3', () => {
+  expect(obj.modulo(3)).toBe('Fizz');
+});
+
+test('modulo enter 4', () => {
+  expect(obj.modulo(4)).toBe(4);
+});
+
+test('modulo enter 5', () => {
+  expect(obj.modulo(5)).toBe('Buzz');
+});
+
+test('modulo enter 6', () => {
+  expect(obj.modulo(6)).toBe('Fizz');
+});
+
+test('modulo enter 7', () => {
+  expect(obj.modulo(7)).toBe(7);
+});
+
+test('modulo enter 8', () => {
+  expect(obj.modulo(8)).toBe(8);
+});
+
+test('modulo enter 9', () => {
+  expect(obj.modulo(9)).toBe('Fizz');
+});
+
+test('modulo enter 10', () => {
+  expect(obj.modulo(10)).toBe('Buzz');
+});
+
+test('modulo enter 11', () => {
+  expect(obj.modulo(11)).toBe(11);
+});
+
+test('modulo enter 12', () => {
+  expect(obj.modulo(12)).toBe('Fizz');
+});
+
+test('modulo enter 13', () => {
+  expect(obj.modulo(13)).toBe(13);
+});
+
+test('modulo enter 14', () => {
+  expect(obj.modulo(14)).toBe(14);
+});
+
+test('modulo enter 15', () => {
+  expect(obj.modulo(15)).toBe('FizzBuzz');
+});
+
+test('modulo enter 16', () => {
+  expect(obj.modulo(16)).toBe(16);
+});
+
+test('modulo enter 17', () => {
+  expect(obj.modulo(17)).toBe(17);
+});
+
+
+
+test('romain number 400', () => {
+  expect(obj.romainNumber(4)).toBe('IV');
+});
+
+test('romain number 3000', () => {
+  expect(obj.romainNumber(3000)).toBe('MMM');
+});
+
+test('romain number 2500', () => {
+  expect(obj.romainNumber(2500)).toBe('MMD');
+});
+
+test('romain number 100', () => {
+  expect(obj.romainNumber(100)).toBe('C');
+});
+
+test('romain number 50', () => {
+  expect(obj.romainNumber(50)).toBe('L');
+});
+
+test('romain number 20', () => {
+  expect(obj.romainNumber(20)).toBe('XX');
+});
+
+test('romain number 5', () => {
+  expect(obj.romainNumber(5)).toBe('V');
+});
+
+test('romain number 3', () => {
+  expect(obj.romainNumber(3)).toBe('III');
+});
+
+test('romain number 2859', () => {
+  expect(obj.romainNumber(2859)).toBe('MMDCCCLIX');
+});
+
+test('romain number 9', () => {
+  expect(obj.romainNumber(9)).toBe('IX');
+});
+
+test('romain number 1995', () => {
+  expect(obj.romainNumber(1995)).toBe('MCMXCV');
+});
+
+test('romain number 853', () => {
+  expect(obj.romainNumber(853)).toBe('DCCCLIII');
+});
+
+test('romain number 400', () => {
+  expect(obj.romainNumber(400)).toBe('CD');
+});
+
+test('romain number 123', () => {
+  expect(obj.romainNumber(123)).toBe('CXXIII');
+});
+
+test('romain number 1', () => {
+  expect(obj.romainNumber(1)).toBe('I');
+});
