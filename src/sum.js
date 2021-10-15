76e24f22aeaa304bc2497904cd7394e41d7456c8
vincent <vincent.clerc@hunimalis.com>
2021-10-15 11:03:06 +0200

    add multiply function

--- src/sum.js
+++ src/sum.js
@@ -4,8 +4,17 @@ function sum(a, b)
     if(isNaN(a) || isNaN(b) ) {
         return false;
     }
-    
+
     return a + b;
 }
 
-module.exports = sum;
+function multiply(a, b)
+{
+    if(isNaN(a) || isNaN(b) ) {
+        return false;
+    }
+    
+    return a * b;
+}
+
+module.exports = {sum, multiply};
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,14 +1,26 @@
 // Add the unit tests of the sum function here
-const sum = require('../src/sum');
+const obj = require('../src/sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(obj.sum(1, 2)).toBe(3);
 });
 
 test('a NAN value', () => {
-  expect(sum('test', 2)).toBe(false);
+  expect(obj.sum('test', 2)).toBe(false);
 });
 
 test('b NAN value', () => {
-  expect(sum(1, 'test')).toBe(false);
+  expect(obj.sum(1, 'test')).toBe(false);
+});
+
+test('adds 5 * 5 to equal 2', () => {
+  expect(obj.multiply(5, 5)).toBe(25);
+});
+
+test('a NAN value', () => {
+  expect(obj.multiply('test', 2)).toBe(false);
+});
+
+test('b NAN value', () => {
+  expect(obj.multiply(1, 'test')).toBe(false);
 });
